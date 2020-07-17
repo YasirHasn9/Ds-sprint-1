a34e08ab813ebbb937eb8f95c7c405a784c7aa12
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-17 09:34:51 -0700

    ring buffer is complete

--- ring_buffer/doublyLInkedList.py
+++ ring_buffer/doublyLInkedList.py
@@ -1,13 +1,40 @@
 class ListNode:
-    def __init__(self, value):
+    def __init__(self, value, prev=None, next=None):
         self.value = value
-        self.prev = None
-        self.next = None
+        self.prev = prev
+        self.next = next
 
 
 class DoublyLinkedList:
     def __init__(self, node=None):
-        self.node = node
-        self.head = None
-        self.tail = None
-        self.length = 1 if not node else 0
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    def add_to_tail(self, value):
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def delete(self, node):
+        self.length -= 1
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head is node:
+            self.head = node.next
+            node.delete()
+        elif self.tail is node:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,16 +1,46 @@
+from doublyLInkedList import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = []
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-
-        if len(self.storage) > self.capacity:
-            self.storage.append(item)
-            self.storage.pop(0)
+        # self.current stores the current oldest element
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            self.current.value = item
+            self.current = self.current.next or self.storage.head
 
     def get(self):
-        l = []
-        for el in self.storage:
-            l.append(el)
-        return l
+        buffers = []
+        cur_node = self.storage.head
+        while cur_node:
+            buffers.append(cur_node.value)
+            cur_node = cur_node.next
+        return buffers
+
+
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+print(buffer.get())   # should return ['d', 'e', 'f']
